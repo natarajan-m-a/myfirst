65e3f2a1d938d7fd48975dbe2a540cf1d7275a2f
natarajan-m-a <73700965+natarajan-m-a@users.noreply.github.com>
2022-08-19 15:34:45 +0530

    copied

--- add.cpp
+++ add.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
-int main(){
-  int a = 5;
-  int b = 5;
-  int c;
-  c = a*b;
-  cout << "Product = " << c;
+
+int main() {
+  double num1, num2, product;
+  cout << "Enter two numbers: ";
+
+  // stores two floating point numbers in num1 and num2 respectively
+  cin >> num1 >> num2;
+ 
+  // performs multiplication and stores the result in product variable
+  product = num1 * num2;  
+
+  cout << "Product = " << product;    
+    
   return 0;
 }
